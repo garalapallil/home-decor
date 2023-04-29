791285b50a85b4f6dd8674169b983f6d9f114028
garalapallil <vedasri@gmail.com~^c>
2023-04-29 17:34:07 +0530

    adding about.php

---
+++ about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Home Decor Website </title>
+</head>
+<body>
+   <h1>Welcome to our homedecor website!</h1>
+   <p>Our website offers a wide range of home decor products, including furniture, lighting, accessories, and more. whether you're looking to refresh your living room, bedroom, or any other room in your house,we have everything you need to make your home beautiful and comfortable.</p>
+   <p> our products are carefully selected to meet the latest trends in home decor, and we work with top brands and designs to offer you the best quality and style.</p>
+   <p>with our easy to use website, you can browse and shop for products online, and have them delivered right to your doorstep. we offer free shipping on orders over $50, and 30-day money -back guarantee to ensure your satisfaction.</p>
+   <p> thank you for visiting our website, and we hope you find what you're looking for to make your home the perfect reflection of your personal style.</p>
+   </body>
+   </html>
